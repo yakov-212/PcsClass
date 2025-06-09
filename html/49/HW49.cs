cc6cf348f9045f88fd171015c2c8895dac2e9801
yakov-212 <jacobabr2795@gmail.com>
2025-06-09 15:35:08 -0400

    HW49 Finished

---
+++ html/49/HW49.cs
@@ -0,0 +1,21 @@
+﻿namespace HW49
+{
+    internal class HW49
+    {
+        static void Main(string[] args)
+        {
+            String[] colors = new String[] { "Red", "Blue", "Green", "Yellow", "Orange", "White"};
+            String[] pattern = new String[] { "stripped", "plain", "plad" };
+            int amount = colors.Length * pattern.Length;
+            Shirt[] closet = new Shirt[amount];
+
+            for (int i = 0; i < amount; i++)
+            {
+                closet[i] = new Shirt(colors[i / pattern.Length],pattern[i % pattern.Length]);
+                Console.Write($"{ i / pattern.Length }, {i % pattern.Length}, ");
+                Console.WriteLine(closet[i]);
+            }
+
+        }
+    }
+}
---
+++ html/49/Shirt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW49
+{
+    internal class Shirt
+    {
+        private String color { set; get; }
+        private String pattern { set; get; }
+        public Shirt(String color, String pattern) { 
+            this.color = color;
+            this.pattern = pattern;
+        }
+        public override string ToString()
+        {
+            return $" {color} {pattern} shirt";
+        }
+    }
+}
