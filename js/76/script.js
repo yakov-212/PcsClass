75d0435401fbd567a95eb7dc609bef28e33d4d9a
yakov-212 <jacobabr2795@gmail.com>
2025-11-04 19:47:23 -0500

    added a second set of pictures

--- js/76/script.js
+++ js/76/script.js
@@ -1,9 +1,9 @@
 (function(){
     'use strict'
-    const images =  JSON.parse(localStorage.getItem('images')) || {}
+    let images;
     const audio = document.querySelector('audio')
     
-    document.querySelector('button').addEventListener("click",e => {
+    document.querySelector('#music').addEventListener("click",e => {
         switch(e.target.innerText){
             case('Music'):
                 e.target.innerText = "Mute"
@@ -16,6 +16,19 @@
                 break;
         }
         
+    })
+    document.querySelector("#set").addEventListener("click",e =>{
+        deleteImgs()
+        switch(e.target.innerText){
+            case('Set 2'):
+                e.target.innerText = "Set 1"
+                load(28,'1',true)
+                break;
+            case('Set 1'):
+                e.target.innerText = "Set 2"
+                load(21,'0',true)
+                break;
+        }
     })
     // const div = document.createElement("div")
     // document.body.appendChild(div)
@@ -30,50 +43,62 @@
     let movement;
     let offset;
     let rotation = 0
-    let n = 0
-    let posLeft = 50
-    let posTop = 400
     
-
-    for (let i = 0; i < 17;i++){
-        const img = document.createElement('img')
-        document.body.appendChild(img)
-        img.style.maskImage = `url(images/${i}.png)`
-        img.index = i
-        img.src = `images/${i}.png`
-        img.style.position = 'absolute'
-        img.style.left = images[i]?.posLeft || `${posLeft}px`
-        img.style.top = images[i]?.posTop || `${posTop}px`
-        images[i] = images[i] || {"posLeft": `${posLeft}px`,"posTop": `${posTop}px`}
-        images[i]['image'] = img
-        posLeft += 190
-        if (i%7 === 0 && i !== 0){
-            posLeft = 50
-            posTop += 250
-        }
-        n+= i%2
-        // images.push(img)
-        // div.appendChild(img)
-        img.addEventListener("mousedown",e =>{
-            e.preventDefault()
-            movement = e.target
-            offset = {x: e.offsetX, y: e.offsetY}
+    function deleteImgs(){
+        const keys = Object.keys(images)
+        keys.forEach(key => {
+            try{
+                images[key].image.remove()
+            }
+            catch{
+                console.log(images[key],images[key].image,key)
+            }
         })
-        img.addEventListener('wheel',e => {
-            e.preventDefault()
-            if(e.wheelDelta > 0){
-                img.style.transform = `rotate(${rotation+=45}deg)`;
-            }  
-            else
-                img.style.transform = `rotate(${rotation-=45}deg)`;
-        })
-        
-
-
     }
-    
+    function load(n,set,clear=false){
+        if (!clear)
+            images =  JSON.parse(localStorage.getItem('images')) || {};
+        else
+            images = {};
 
-    
+        let posLeft = 100;
+        let posTop = 0;
+
+        for (let i = 0; i < n;i++){
+            
+            const img = document.createElement('img')
+            document.body.appendChild(img)
+            img.style.maskImage = `url(images/${set}/${i}.png)`
+            img.index = i
+            img.src = `images/${set}/${i}.png`
+            img.style.position = 'absolute'
+            img.style.left = images[i]?.posLeft || `${posLeft}px`
+            img.style.top = images[i]?.posTop || `${posTop}px`
+            images[i] = images[i] || {"posLeft": `${posLeft}px`,"posTop": `${posTop}px`}
+            images[i]['image'] = img
+            posLeft += 190
+            if (i%6 === 0 && i !== 0){
+                posLeft = 50
+                posTop += 250
+            }
+            // images.push(img)
+            // div.appendChild(img)
+            img.addEventListener("mousedown",e =>{
+                e.preventDefault()
+                movement = e.target
+                offset = {x: e.offsetX, y: e.offsetY}
+            })
+            img.addEventListener('wheel',e => {
+                e.preventDefault()
+                if(e.wheelDelta > 0){
+                    img.style.transform = `rotate(${rotation+=45}deg)`;
+                }  
+                else
+                    img.style.transform = `rotate(${rotation-=45}deg)`;
+            })
+        }
+    }    
+    load(21,'0')
     document.addEventListener("mousemove",e => {
         if(movement){
             movement.style.left = `${e.pageX - offset.x}px`;
@@ -81,8 +106,13 @@
         }
     })
     document.addEventListener("mouseup",e =>{
+        try{
         images[movement.index].posLeft = movement.style.left
         images[movement.index].posTop = movement.style.top
+        }
+        catch{
+            console.log(movement)
+        }
         
         localStorage.setItem("images",JSON.stringify(images))
 
