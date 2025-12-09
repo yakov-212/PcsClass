5178cb82e32894316dbeef740edbd373bf6bebca
yakov-212 <jacobabr2795@gmail.com>
2025-12-09 13:34:39 -0500

    finnished weather

---
+++ js/85/Weather/src/Weather.jsx
@@ -0,0 +1,31 @@
+import { Component } from "react";
+import './Weather.css'
+export default class Weather extends Component{
+    state = {
+        weatherData: '',
+        icon: null
+    }
+    
+    
+     componentDidMount = async ()=>{
+        try{
+            const weather = await (await fetch('https://api.weather.gov/gridpoints/TOP/32,81/forecast/hourly')).json()
+            this.setState({
+                weatherData: weather.properties.periods[0].shortForecast,
+                icon: weather.properties.periods[0].icon
+            })
+            console.log(weather)
+        }
+        catch{console.log('hey')}
+    }
+    render(){
+        return (
+            <div className="weather">
+                <div>Weather</div>
+                <img src={this.state?.icon}></img>
+                <div>{this.state.weatherData}</div>
+                {console.log(this.state)}
+            </div>
+        )
+    }
+}
