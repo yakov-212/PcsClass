9d4326be2e0b297d600aedb3a1609ba4a66172b8
yakov-212 <jacobabr2795@gmail.com>
2025-12-09 20:27:25 -0500

    'k'

--- js/86/calculator/src/Calculator.jsx
+++ js/86/calculator/src/Calculator.jsx
@@ -48,6 +48,7 @@ export default class Calculator extends Component{
         console.log(this.state.num)
         if (this.resultsRef.current) {
           this.resultsRef.current.scrollLeft = this.resultsRef.current.scrollWidth;
+          console.log(this.resultsRef.current.scrollWidth)
         }
     }
 
